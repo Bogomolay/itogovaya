b63f7dc1de6fe01e1edffbe21a41ef713877126c
Anna <b.anna.94@mail.ru>
2022-10-12 19:18:05 +0500

    Zadacha

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] text(string[] array)
+    {           
+        string[] result = new string[array.Length];
+        int count = 0;
+            
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i].Length <= 3)
+            {
+                result[count] = array[i];
+                count++;
+            }
+        }
+        Array.Resize(ref result, count);
+            
+        return result;
+    }
+        
+    void PrintArray(string[] array)
+    {
+        Console.WriteLine("[" + string.Join(", ", array) + "]");
+        Console.WriteLine();
+    }
+    
+    string[] array1 = {"hello", "2", "world", ":-)"};
+    string[] array2 = {"1234", "1567", "-2", "computer science"};
+    string[] array3 = {"Russia", "Denmark", "Kazan"};
+
+        PrintArray(text(array1));
+        PrintArray(text(array2));
+        PrintArray(text(array3));
